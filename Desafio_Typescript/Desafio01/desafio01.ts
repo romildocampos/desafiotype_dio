736de5b7cb0204cfa2550bccc93c3b4b4d833f5a
Romildo de Campos <camposrsp@gmail.com>
2022-07-06 13:28:33 -0300

    Add desafio

---
+++ Desafio_Typescript/Desafio01/desafio01.ts
@@ -0,0 +1,11 @@
+//Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+//let employee = {};
+//employee.code = 10;
+//employee.name = "John";
+
+
+const funcionario2: {codigo: number, nome: string} = {
+    codigo: 10,
+    nome: 'joao'
+}
---
+++ Desafio_Typescript/Desafio01/dist/desafio01.js
@@ -0,0 +1,11 @@
+"use strict";
+//Como podemos rodar isso em um arquivo .ts sem causar erros? 
+//let employee = {};
+//employee.code = 10;
+//employee.name = "John";
+
+
+const funcionario2 = {
+    codigo: 10,
+    nome: 'joao'
+};
